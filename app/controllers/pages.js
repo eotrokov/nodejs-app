1078b19caebeaf71ec01137f1504fc88d801a463
Отроков Евгений <otrokov@me.com>
2018-06-14 17:24:57 +0300

    add pg

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -1,3 +1,8 @@
-exports.home = function (req, res) {
-  res.send(req.query)
+const { Client } = require('pg')
+exports.home = async function (req, res) {
+  const client = new Client({ user: 'postgres', host: '192.168.1.75', database: 'License', password: '*', port: 5432 })
+  await client.connect()
+  const res1 = await client.query(`select * from \"Users\" where \"Token\" = \'${req.param('token')}\'`)
+  res.send(res1.rows[0]) // Hello world!
+  await client.end()
 }
